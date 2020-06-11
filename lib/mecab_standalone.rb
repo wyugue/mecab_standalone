58266dde53b480043d012e9d41f006155fbbec10
Yuge <williamyugue@gmail.com>
2020-06-11 11:54:52 -0300

    fix LD_LIBRARY_PATH typo

--- lib/mecab_standalone.rb
+++ lib/mecab_standalone.rb
@@ -1,8 +1,8 @@
 class MecabStandalone
-  File.join( $LOAD_PATH.grep(/mecab\_standalone\-/).first, 'lib' )
+  LD_LIBRARY_PATH = File.join( $LOAD_PATH.grep(/mecab\_standalone\-/).first, 'lib' )
 
   def self.yomi(input, rcfile = "#{__dir__ }/etc/mecabrc", dic = "#{__dir__ }/lib/mecab/dic/ipadic/")
     # `echo #{input} | #{__dir__ }/bin/mecab -r #{rcfile} -d #{dic} -Oyomi`
-    IO.popen({'LD_LIBRAY_PATH' => LD_LIBRAY_PATH}, "echo #{input} | #{__dir__ }/bin/mecab -r #{rcfile} -d #{dic} -Oyomi").read
+    IO.popen({'LD_LIBRARY_PATH' => LD_LIBRARY_PATH}, "echo #{input} | #{__dir__ }/bin/mecab -r #{rcfile} -d #{dic} -Oyomi").read
   end
 end
