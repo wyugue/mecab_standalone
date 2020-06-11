2e945ec9f9a93dfe69c2ebd8a25fe7b97e1c5a55
Yuge <williamyugue@gmail.com>
2020-06-11 19:45:46 -0300

    Change yomi to reading

--- lib/mecab_standalone.rb
+++ lib/mecab_standalone.rb
@@ -1,8 +1,7 @@
 class MecabStandalone
   LD_LIBRARY_PATH = File.join( $LOAD_PATH.grep(/mecab\_standalone\-/).first, 'lib' )
 
-  def self.yomi(input, rcfile = "#{__dir__ }/etc/mecabrc", dic = "#{__dir__ }/lib/mecab/dic/ipadic/")
-    # `echo #{input} | #{__dir__ }/bin/mecab -r #{rcfile} -d #{dic} -Oyomi`
+  def self.reading(input, rcfile = "#{__dir__ }/etc/mecabrc", dic = "#{__dir__ }/lib/mecab/dic/ipadic/")
     IO.popen({'LD_LIBRARY_PATH' => LD_LIBRARY_PATH}, "echo #{input} | #{__dir__ }/bin/mecab -r #{rcfile} -d #{dic} -Oyomi").read
   end
 end
