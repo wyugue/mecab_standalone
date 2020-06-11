df31ff93149a703f52eaf4e285e83505527e9861
Yuge <williamyugue@gmail.com>
2020-06-11 11:51:20 -0300

    add /lib

--- lib/mecab_standalone.rb
+++ lib/mecab_standalone.rb
@@ -1,5 +1,5 @@
 class MecabStandalone
-  LD_LIBRAY_PATH = $LOAD_PATH.grep(/mecab\_standalone\-/).first
+  LD_LIBRAY_PATH = $LOAD_PATH.grep(/mecab\_standalone\-/).first + '/lib'
 
   def self.yomi(input, rcfile = "#{__dir__ }/etc/mecabrc", dic = "#{__dir__ }/lib/mecab/dic/ipadic/")
     # `echo #{input} | #{__dir__ }/bin/mecab -r #{rcfile} -d #{dic} -Oyomi`
