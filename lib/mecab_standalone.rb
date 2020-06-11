85fbf9c15a364a4c4b0cade54036eab41fd789ba
Yuge <williamyugue@gmail.com>
2020-06-11 11:44:26 -0300

    Add dynamic library path

--- lib/mecab_standalone.rb
+++ lib/mecab_standalone.rb
@@ -1,9 +1,8 @@
 class MecabStandalone
+  LD_LIBRAY_PATH = $LOAD_PATH.grep(/mecab\_standalone\-/).first
+
   def self.yomi(input, rcfile = "#{__dir__ }/etc/mecabrc", dic = "#{__dir__ }/lib/mecab/dic/ipadic/")
-    `echo #{input} | #{__dir__ }/bin/mecab -r #{rcfile} -d #{dic} -Oyomi`
+    # `echo #{input} | #{__dir__ }/bin/mecab -r #{rcfile} -d #{dic} -Oyomi`
+    IO.popen({'LD_LIBRAY_PATH' => path}, "echo #{input} | #{__dir__ }/bin/mecab -r #{rcfile} -d #{dic} -Oyomi").read
   end
 end
-
-
-# lib/bin/mecab -d lib/lib/mecab/dic/ipadic/
-# echo "はじめに" | lib/bin/mecab -d lib/lib/mecab/dic/ipadic/ -Oyomi
