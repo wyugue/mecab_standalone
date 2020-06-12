6598f073dde48090316f3b0e48935156ee9605a0
Yuge <williamyugue@gmail.com>
2020-06-11 23:29:12 -0300

    Clean input string

--- lib/mecab_standalone.rb
+++ lib/mecab_standalone.rb
@@ -1,7 +1,8 @@
 class MecabStandalone
   LD_LIBRARY_PATH = File.join( $LOAD_PATH.grep(/mecab\_standalone\-/).first, 'lib' )
 
-  def self.reading(input, rcfile = "#{__dir__ }/etc/mecabrc", dic = "#{__dir__ }/lib/mecab/dic/ipadic/")
-    IO.popen({'LD_LIBRARY_PATH' => LD_LIBRARY_PATH}, "echo \"#{input}\" | #{__dir__ }/bin/mecab -r #{rcfile} -d #{dic} -Oyomi").read
+  def self.reading(input, rcfile = "#{__dir__ }/etc/mecabrc", dic = "#{__dir__ }/lib/mecab/dic/ipadic/")    
+    IO.popen({'LD_LIBRARY_PATH' => LD_LIBRARY_PATH}, 
+      "echo \"#{input.delete("()\"'")}\" | #{__dir__ }/bin/mecab -r #{rcfile} -d #{dic} -Oyomi").read
   end
 end
