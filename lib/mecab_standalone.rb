da46675275263d38c9c80c622b91209816940e90
Yuge <williamyugue@gmail.com>
2020-06-12 18:53:58 -0300

    Add io.close

--- lib/mecab_standalone.rb
+++ lib/mecab_standalone.rb
@@ -2,7 +2,10 @@ class MecabStandalone
   LD_LIBRARY_PATH = File.join( $LOAD_PATH.grep(/mecab\_standalone\-/).first, 'lib' )
 
   def self.reading(input, rcfile = "#{__dir__ }/etc/mecabrc", dic = "#{__dir__ }/lib/mecab/dic/ipadic/")    
-    IO.popen({'LD_LIBRARY_PATH' => LD_LIBRARY_PATH}, 
-      "echo \"#{input}\" | #{__dir__ }/bin/mecab -r #{rcfile} -d #{dic} -Oyomi").read
+    io = IO.popen({'LD_LIBRARY_PATH' => LD_LIBRARY_PATH}, 
+      "echo \"#{input}\" | #{__dir__ }/bin/mecab -r #{rcfile} -d #{dic} -Oyomi")
+    reading = io.read
+    io.close
+    reading
   end
 end
