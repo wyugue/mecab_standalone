90b8a2487aebbeaee988a4479edefd4beedf1981
Yuge <williamyugue@gmail.com>
2020-06-12 14:38:23 -0300

    Remove delete from mecab

--- lib/mecab_standalone.rb
+++ lib/mecab_standalone.rb
@@ -3,6 +3,6 @@ class MecabStandalone
 
   def self.reading(input, rcfile = "#{__dir__ }/etc/mecabrc", dic = "#{__dir__ }/lib/mecab/dic/ipadic/")    
     IO.popen({'LD_LIBRARY_PATH' => LD_LIBRARY_PATH}, 
-      "echo \"#{input.delete("()\"'")}\" | #{__dir__ }/bin/mecab -r #{rcfile} -d #{dic} -Oyomi").read
+      "echo \"#{input}\" | #{__dir__ }/bin/mecab -r #{rcfile} -d #{dic} -Oyomi").read
   end
 end
