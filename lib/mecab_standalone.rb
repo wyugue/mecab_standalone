8ef68f8f745168d995d840a3074a4ca7d3c36553
yuge <williamyugue@gmail.com>
2020-09-08 19:05:51 -0300

    Fix gemspec and bump version

--- lib/mecab_standalone.rb
+++ lib/mecab_standalone.rb
@@ -1,9 +1,17 @@
 class MecabStandalone
   LD_LIBRARY_PATH = File.join( $LOAD_PATH.grep(/mecab\_standalone\-/).first, 'lib' )
 
-  def self.reading(input, rcfile = "#{__dir__ }/etc/mecabrc", dic = "#{__dir__ }/lib/mecab/dic/ipadic/")    
-    io = IO.popen({'LD_LIBRARY_PATH' => LD_LIBRARY_PATH}, 
+  def self.reading(input, rcfile = "#{__dir__ }/etc/mecabrc", dic = "#{__dir__ }/lib/mecab/dic/ipadic/")
+    io = IO.popen({'LD_LIBRARY_PATH' => LD_LIBRARY_PATH},
       "echo \"#{input}\" | #{__dir__ }/bin/mecab -r #{rcfile} -d #{dic} -Oyomi")
+    reading = io.read
+    io.close
+    reading
+  end
+
+  def self.parse(input, rcfile = "#{__dir__ }/etc/mecabrc", dic = "#{__dir__ }/lib/mecab/dic/ipadic/")
+    io = IO.popen({'LD_LIBRARY_PATH' => LD_LIBRARY_PATH},
+      "echo \"#{input}\" | #{__dir__ }/bin/mecab -r #{rcfile} -d #{dic}")
     reading = io.read
     io.close
     reading
--- mecab_standalone.gemspec
+++ mecab_standalone.gemspec
@@ -2,14 +2,14 @@ $LOAD_PATH.push File.expand_path("../lib", __FILE__)
 
 Gem::Specification.new do |s|
   s.name        = 'mecab_standalone'
-  s.version     = '0.1.1'
-  s.date        = '2020-09-03'
+  s.version     = '0.1.2'
+  s.date        = '2020-09-08'
   s.summary     = 'mecab_standalone'
-  s.description = 'A simple Mecab stand alone gem'
+  s.description = 'A simple Mecab standalone gem'
   s.authors     = ['Taku Kudo']
   s.email       = 'taku@chasen.org'
   s.license     = 'MIT'
-  s.homepage    = 'https://taku910.github.io/mecab/'
+  s.homepage    = 'https://github.com/wyugue/mecab_standalone'
   s.files       = ['lib/mecab_standalone.rb']
   s.files      += ['lib/libmecab.so.2.0.0']
   s.files      += Dir['lib/bin/*']
